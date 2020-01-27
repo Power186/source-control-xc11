179851607ed3dcaa9059852c362ec4fd8e6bf900
Scott <agentwinburn1@gmail.com>
2020-01-27 19:38:10 +0800

    Initial Commit

---
+++ second/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  second
+//
+//  Created by Power186 on 1/27/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ second/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  second
+//
+//  Created by Power186 on 1/27/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
